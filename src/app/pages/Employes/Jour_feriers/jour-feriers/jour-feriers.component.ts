addc980508c2a1ecd6600112c304f50bb61ef35e
leonlogin <leonelkotieu@gmail.com>
2021-09-06 18:00:37 +0100

    commit to present other feature

--- src/app/pages/Employes/Jour_feriers/jour-feriers/jour-feriers.component.ts
+++ src/app/pages/Employes/Jour_feriers/jour-feriers/jour-feriers.component.ts
@@ -7,28 +7,29 @@ export interface PeriodicElement {
   position: number;
   weight: number;
   symbol: string;
+  symbole: string;
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H', symbole: 'H'},
+  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He', symbole: 'H'},
+  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li', symbole: 'H'},
+  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be', symbole: 'H'},
+  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B', symbole: 'H'},
+  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C', symbole: 'H'},
+  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N', symbole: 'H'},
+  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O', symbole: 'H'},
+  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F', symbole: 'H'},
+  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne', symbole: 'H'},
 ];
 
 @Component({
   selector: 'app-jour-feriers',
   templateUrl: './jour-feriers.component.html',
-  styleUrls: ['./jour-feriers.component.css']  
+  styleUrls: ['./jour-feriers.component.css']
 })
 export class JourFeriersComponent implements OnInit {
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol', 'symbole'];
   dataSource = ELEMENT_DATA;
 
   constructor() { }
